a05a43b1bf5e64208e5142c944d3dceaf6c0fb0a
Luca-Panza <luca.panza.007@gmail.com>
2023-08-24 17:40:47 -0300

    feat: add route to fetch orders by client

--- src/controllers/clients.controllers.js
+++ src/controllers/clients.controllers.js
@@ -1,4 +1,4 @@
-import { insertClient } from "../repositories/clients.repositories.js";
+import { insertClient, getClientByIdQuery, getOrdersByClientId } from "../repositories/clients.repositories.js";
 
 export async function createClient(req, res) {
   const { name, address, phone } = req.body;
@@ -7,6 +7,30 @@ export async function createClient(req, res) {
     await insertClient(name, address, phone);
 
     res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function getOrdersByClient(req, res) {
+  const { id } = req.params;
+
+  try {
+    const existingClient = await getClientByIdQuery(id);
+    if (existingClient.rowCount === 0) return res.status(404).send({ message: "Client not found" });
+
+    const existingOrders = await getOrdersByClientId(id);
+    if (existingOrders.rows.length === 0) return res.status(404).send({ message: "No orders found" });
+
+    const orders = existingOrders.rows.map((order) => ({
+      orderId: order.orderId,
+      quantity: order.quantity,
+      createdAt: order.createdAt,
+      totalPrice: parseFloat(order.totalPrice),
+      cakeName: order.cakeName,
+    }));
+
+    res.status(200).send(orders);
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { clientSchema } from "../schemas/clients.schema.js";
-import { createClient } from "../controllers/clients.controllers.js";
+import { createClient, getOrdersByClient } from "../controllers/clients.controllers.js";
 
 const clientRouter = Router();
 
 clientRouter.post("/clients", validateSchema(clientSchema), createClient);
+clientRouter.get("/clients/:id/orders", getOrdersByClient);
 
 export default clientRouter;
